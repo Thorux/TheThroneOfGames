d0efa3ca224411d6c47b3fa2fca06f06f9974f20
Thorux <anders@regnbue.org>
2015-02-21 03:48:12 +0100

    FK YEAH! Working build +1, kappa

---
+++ src/Data/Whatever_2.java
@@ -0,0 +1,29 @@
+package Data;
+
+/**
+ * Created by Anders on 21-02-2015.
+ */
+public class Whatever_2 {
+
+    public static int row, col;
+    public static
+
+
+
+    public static void main(String[] args){
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
