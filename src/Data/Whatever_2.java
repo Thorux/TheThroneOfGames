6eb137eca29054b8d8ba6fd31617ab829e101477
Thorux <anders@regnbue.org>
2015-02-21 04:14:02 +0100

    FK YEAH! Working build +1, kappa

--- src/Data/Whatever_2.java
+++ src/Data/Whatever_2.java
@@ -1,17 +1,39 @@
 package Data;
 
-import static Helpers.Artists.*;
+import java.util.Scanner;
+
+import static Helpers.Artists.BeginSession;
+
 /**
  * Created by Anders on 21-02-2015.
  */
 public class Whatever_2 {
 
     public static int row, col;
+    public static Scanner scan = new Scanner(System.in);
+    public static int[][] board = new int[6][7];
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         BeginSession();
+
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                board[i][j] = 0;
+            }
+        }
     }
+
+    public static void Play() {
+
+    }
+
+    public static void PrintBoard() {
+
+    }
+
+
+
 
 
 }
